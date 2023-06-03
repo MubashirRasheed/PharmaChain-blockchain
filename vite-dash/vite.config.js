7ea4af1e24dcce21c0a989a405a513a25e9b431d
MubashirRasheed <sakuramitsutsuki002@gmail.com>
2023-06-03 20:00:44 +0500

    vite config reverted

--- vite-dash/vite.config.js
+++ vite-dash/vite.config.js
@@ -1,42 +1,42 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc';
-// import * as esbuild from "esbuild";
-// import fs from "node:fs";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import * as esbuild from 'esbuild';
+import fs from 'node:fs';
 
-// // https://vitejs.dev/config/
+// https://vitejs.dev/config/
 
-// // const rollupPlugin = (matchers) => ({
-// //   name: "js-in-jsx",
-// //   load(id) {
-// //     if (matchers.some(matcher => matcher.test(id))) {
-// //       const file = fs.readFileSync(id, { encoding: "utf-8" });
-// //       return esbuild.transformSync(file, { loader: "jsx" });
-// //     }
-// //   }
-// // });
-
-// export default defineConfig({
-//   plugins: [react(),
-
-//     {
-//         name: "load-js-files-as-jsx",
-//         async load(id) {
-//             if (!id.match(/src\/.*\.js$/)) {
-//                 return
-//             }
-
-//             const file = fs.readFileSync(id, { encoding: "utf-8" });
-//             return esbuild.transformSync(file, { loader: "jsx" })
-//         }
-//     }
-// ],
-// esbuild: {
-//     // add this configuration object for the jsx loader
-//     loader: {
-//       ".js": "jsx"
+// const rollupPlugin = (matchers) => ({
+//   name: "js-in-jsx",
+//   load(id) {
+//     if (matchers.some(matcher => matcher.test(id))) {
+//       const file = fs.readFileSync(id, { encoding: "utf-8" });
+//       return esbuild.transformSync(file, { loader: "jsx" });
 //     }
 //   }
-// })
+// });
+
+export default defineConfig({
+  plugins: [react(),
+
+    {
+      name: 'load-js-files-as-jsx',
+      async load(id) {
+        if (!id.match(/src\/.*\.js$/)) {
+          return;
+        }
+
+        const file = fs.readFileSync(id, { encoding: 'utf-8' });
+        return esbuild.transformSync(file, { loader: 'jsx' });
+      },
+    },
+  ],
+  esbuild: {
+    // add this configuration object for the jsx loader
+    loader: {
+      '.js': 'jsx',
+    },
+  },
+});
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react-swc';
@@ -66,49 +66,36 @@
 //   }
 // })
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import fs from 'fs';
-import { transformSync } from 'esbuild';
-import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
-import requireTransform from 'vite-plugin-require-transform';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import fs from 'fs';
+// import { transformSync } from 'esbuild';
+// import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
-export default defineConfig({
-  plugins: [
-    viteCommonjs(),
-    react(),
-    requireTransform({
-      transforms: [
-        {
-          test: /src\/.*\.(js|jsx)$/,
-          transform: (code) =>
-            // Modify the code here if needed
-            code
-          ,
-        },
-      ],
-    }),
-    {
-      name: 'load-js-files-as-jsx',
-      async load(id) {
-        if (!id.match(/src\/.*\.(js|jsx)$/)) {
-          return;
-        }
+// export default defineConfig({
+//   plugins: [
+//     viteCommonjs(),
+//     react(),
+//     {
+//       name: 'load-js-files-as-jsx',
+//       async load(id) {
+//         if (!id.match(/src\/.*\.(js|jsx)$/)) {
+//           return;
+//         }
 
-        const file = fs.readFileSync(id, { encoding: 'utf-8' });
-        const result = transformSync(file, { loader: 'jsx' });
+//         const file = fs.readFileSync(id, { encoding: 'utf-8' });
+//         const result = transformSync(file, { loader: 'jsx' });
 
-        return {
-          code: result.code,
-          map: result.map,
-        };
-      },
-    },
-  ],
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(['react-s3'])],
-    },
-  },
-});
-
+//         return {
+//           code: result.code,
+//           map: result.map,
+//         };
+//       },
+//     },
+//   ],
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       plugins: [esbuildCommonjs(['react-s3'])],
+//     },
+//   },
+// });
