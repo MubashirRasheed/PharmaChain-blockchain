0a2da5cb9e714c2e8f6b9ae51625757c0423e232
MubashirRasheed <sakuramitsutsuki002@gmail.com>
2023-06-03 20:53:31 +0500

    new vite config

--- vite-dash/vite.config.js
+++ vite-dash/vite.config.js
@@ -1,41 +1,54 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import * as esbuild from 'esbuild';
-import fs from 'node:fs';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import * as esbuild from 'esbuild';
+// import fs from 'node:fs';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
 
-// const rollupPlugin = (matchers) => ({
-//   name: "js-in-jsx",
-//   load(id) {
-//     if (matchers.some(matcher => matcher.test(id))) {
-//       const file = fs.readFileSync(id, { encoding: "utf-8" });
-//       return esbuild.transformSync(file, { loader: "jsx" });
-//     }
-//   }
+// // const rollupPlugin = (matchers) => ({
+// //   name: "js-in-jsx",
+// //   load(id) {
+// //     if (matchers.some(matcher => matcher.test(id))) {
+// //       const file = fs.readFileSync(id, { encoding: "utf-8" });
+// //       return esbuild.transformSync(file, { loader: "jsx" });
+// //     }
+// //   }
+// // });
+
+// export default defineConfig({
+//   plugins: [react(),
+
+//     {
+//       name: 'load-js-files-as-jsx',
+//       async load(id) {
+//         if (!id.match(/src\/.*\.js$/)) {
+//           return;
+//         }
+
+//         const file = fs.readFileSync(id, { encoding: 'utf-8' });
+//         return esbuild.transformSync(file, { loader: 'jsx' });
+//       },
+//     },
+//   ],
+//   esbuild: {
+//     // add this configuration object for the jsx loader
+//     loader: 'jsx',
+//     // loader: {
+//     //   '.js': 'jsx',
+//     // },
+//   },
 // });
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-
-    {
-      name: 'load-js-files-as-jsx',
-      async load(id) {
-        if (!id.match(/src\/.*\.js$/)) {
-          return;
-        }
-
-        const file = fs.readFileSync(id, { encoding: 'utf-8' });
-        return esbuild.transformSync(file, { loader: 'jsx' });
-      },
-    },
-  ],
-  esbuild: {
-    // add this configuration object for the jsx loader
-    loader: 'jsx',
-    // loader: {
-    //   '.js': 'jsx',
-    // },
+  plugins: [react()],
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
   },
 });
-
