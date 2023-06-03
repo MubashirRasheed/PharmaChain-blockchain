b2709e01c817fe7df6004b9e51426c7958644bf1
MubashirRasheed <sakuramitsutsuki002@gmail.com>
2023-06-03 20:01:49 +0500

    trying without config

--- vite-dash/vite.config.js
+++ vite-dash/vite.config.js
@@ -1,101 +1,40 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import * as esbuild from 'esbuild';
-import fs from 'node:fs';
-
-// https://vitejs.dev/config/
-
-// const rollupPlugin = (matchers) => ({
-//   name: "js-in-jsx",
-//   load(id) {
-//     if (matchers.some(matcher => matcher.test(id))) {
-//       const file = fs.readFileSync(id, { encoding: "utf-8" });
-//       return esbuild.transformSync(file, { loader: "jsx" });
-//     }
-//   }
-// });
-
-export default defineConfig({
-  plugins: [react(),
-
-    {
-      name: 'load-js-files-as-jsx',
-      async load(id) {
-        if (!id.match(/src\/.*\.js$/)) {
-          return;
-        }
-
-        const file = fs.readFileSync(id, { encoding: 'utf-8' });
-        return esbuild.transformSync(file, { loader: 'jsx' });
-      },
-    },
-  ],
-  esbuild: {
-    // add this configuration object for the jsx loader
-    loader: {
-      '.js': 'jsx',
-    },
-  },
-});
-
-// import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react-swc';
-// import * as esbuild from "esbuild";
-// import fs from "node:fs";
+// import * as esbuild from 'esbuild';
+// import fs from 'node:fs';
+
+// // https://vitejs.dev/config/
+
+// // const rollupPlugin = (matchers) => ({
+// //   name: "js-in-jsx",
+// //   load(id) {
+// //     if (matchers.some(matcher => matcher.test(id))) {
+// //       const file = fs.readFileSync(id, { encoding: "utf-8" });
+// //       return esbuild.transformSync(file, { loader: "jsx" });
+// //     }
+// //   }
+// // });
 
 // export default defineConfig({
 //   plugins: [react(),
 
 //     {
-//         name: 'load-js-files-as-jsx',
-//         async load(id) {
-//             if (!id.match(/src\/.*\.js$/)) {
-//                 return
-//             }
-
-//             const file = fs.readFileSync(id, { encoding: "utf-8" });
-//             return esbuild.transformSync(file, { loader: 'jsx' })
-//         }
-//     }
-// ],
-// esbuild: {
-//     // add this configuration object for the jsx loader
-//     loader: {
-//       '.js': 'jsx'
-//     }
-//   }
-// })
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
-// import fs from 'fs';
-// import { transformSync } from 'esbuild';
-// import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
-
-// export default defineConfig({
-//   plugins: [
-//     viteCommonjs(),
-//     react(),
-//     {
 //       name: 'load-js-files-as-jsx',
 //       async load(id) {
-//         if (!id.match(/src\/.*\.(js|jsx)$/)) {
+//         if (!id.match(/src\/.*\.js$/)) {
 //           return;
 //         }
 
 //         const file = fs.readFileSync(id, { encoding: 'utf-8' });
-//         const result = transformSync(file, { loader: 'jsx' });
-
-//         return {
-//           code: result.code,
-//           map: result.map,
-//         };
+//         return esbuild.transformSync(file, { loader: 'jsx' });
 //       },
 //     },
 //   ],
-//   optimizeDeps: {
-//     esbuildOptions: {
-//       plugins: [esbuildCommonjs(['react-s3'])],
+//   esbuild: {
+//     // add this configuration object for the jsx loader
+//     loader: {
+//       '.js': 'jsx',
 //     },
 //   },
 // });
+
